4b51fd92ba89d0eda284bd76e8c1c77a9a0d48bb
TshepoDavid Molefe <tshepomolefeda@gmail.com>
2021-08-13 19:57:12 +0200

    Modify public html

--- src/App.js
+++ src/App.js
@@ -4,5 +4,9 @@ import Calculator from './components/Calculator';
 import './components/Calculator.css';
 
 class App extends React.Component {
+  render() {
+    return <Calculator />;
+  }
+}
 
 export default App;
