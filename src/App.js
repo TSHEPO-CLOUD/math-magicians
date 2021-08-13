07301ad0770d4db47e07ef5ddde9aad8a75deb5d
TshepoDavid Molefe <tshepomolefeda@gmail.com>
2021-08-13 19:56:37 +0200

    Import Culculator css and js in app.js

--- src/App.js
+++ src/App.js
@@ -4,9 +4,5 @@ import Calculator from './components/Calculator';
 import './components/Calculator.css';
 
 class App extends React.Component {
-  render() {
-    return <Calculator />;
-  }
-}
 
 export default App;
