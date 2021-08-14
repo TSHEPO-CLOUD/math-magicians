59c547882266d7558e623ed590cd50e176994620
TshepoDavid Molefe <tshepomolefeda@gmail.com>
2021-08-14 10:35:37 +0200

    Add operetat funtion

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -5,4 +5,22 @@ export default function operate(numberOne, numberTwo, operation) {
   const two = Big(numberTwo);
   if (operation === '+') {
     return one.plus(two).toString();
-  
+  }
+  if (operation === '-') {
+    return one.minus(two).toString();
+  }
+  if (operation === 'x') {
+    return one.times(two).toString();
+  }
+  if (operation === '÷') {
+    try {
+      return one.div(two).toString();
+    } catch {
+      return 'Error!';
+    }
+  }
+  if (operation === '%') {
+    return one.mod(two).toString();
+  }
+  throw Error(`Unknown operation '${operation}'`);
+}
